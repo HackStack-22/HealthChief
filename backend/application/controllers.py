e76b66a9d71edd954265877d9cec1c5161f3da7f
Rahul M <roboto7o32oo3@gmail.com>
2022-04-10 18:37:50 +0530

    added endpoint for diabetes predict

--- backend/application/controllers.py
+++ backend/application/controllers.py
@@ -3,6 +3,7 @@ from flask import request, render_template, redirect, url_for
 import joblib
 import os
 
+import pickle
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
@@ -18,6 +19,35 @@ def home():
 @app.route("/test")
 def test():
     return "hello world changed", 200
+
+@app.route("/ml_model/diabetes/predict", methods = ["GET", "POST"])
+def diabetes_predict():
+    if request.method == 'POST':
+        form = request.form
+        Pregnancies = form.get('pregnancies')
+        Glucose = form.get('glucose')
+        BloodPressure = form.get('blood-pressure')
+        SkinThickness = form.get('skin-thickness')
+        Insulin = form.get('insulin')
+        BMI = form.get('bmi')
+        DiabetesPedigreeFunction = form.get('diabetes-pedigree-function')
+        Age = form.get('age')
+        
+        arr = [Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]
+        
+        df = pd.DataFrame(
+            [arr],
+            columns=['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI', 'DiabetesPedigreeFunction', 'Age']
+        )      
+        
+        model = joblib.load(os.path.join(ml_models_dir, 'diabetes.pkl'))
+        predict = model.predict(df)
+        print("The predicted result:", predict)
+        return str(predict[0]), 200
+    
+    return 'Diabetes Predict', 200
+        
+        
 
 @app.route("/ml_model/heart_failure/predict", methods = ["GET","POST"])
 def heart_failure_predict():
@@ -63,9 +93,127 @@ def hepatitis_c_predict():
     
     return 'Hepatitis C Predict', 200
 
-@app.route("/ml_model/breast_cancer/predict", methods = ["GET", "POST"])
-def breast_cancer_predict():
+
+
+def data_pipeline(df):
+    yes_no_cols = [
+        'HeartDisease',
+        'Smoking',
+        'AlcoholDrinking',
+        'Stroke',
+        'DiffWalking',
+        'Diabetic',
+        'PhysicalActivity',
+    ]
+    male_female_cols = ['Sex']
+    race_cols = ['Race']
+    gen_health = ['GenHealth']
+    age_cols = ['AgeCategory']
+    numeric_cols = ['BMI', 'PhysicalHeath', 'MentalHealth', 'SleepTime']
+
+    for cols in yes_no_cols:
+        df[cols] = df[cols].map({
+            'Yes' : 1, 
+            'No'  : 0
+        })
+
+    for cols in male_female_cols:
+        df[cols] = df[cols].map({
+            'Male' : 1, 
+            'Female'  : 0
+        })
+
+    for cols in age_cols:
+        df[cols] = df[cols].map({
+            '18-24': 21064,
+            '25-29': 16955,
+            '30-34': 18753,
+            '35-39': 20550,
+            '40-44': 21006,
+            '45-49': 21791,
+            '50-54': 25382,
+            '55-59': 29757,
+            '60-64': 33686,
+            '65-69': 34151,
+            '70-74': 31065,
+            '75-79': 21482,
+            '80 or older': 24153
+        })
+
+    for cols in race_cols:
+        df[cols] = df[cols].map({
+            'American Indian/Alaskan Native': 5202,
+            'Asian': 8068,
+            'Black': 22939,
+            'Hispanic': 27446,
+            'Other': 10928,
+            'White': 245212
+        })
+
+    for cols in gen_health:
+        df[cols] = df[cols].map({
+            'Excellent': 66842,
+            'Fair': 34677,
+            'Good': 93129,
+            'Poor': 11289,
+            'Very good': 113858
+        })
+    return df
+
+
+@app.route("/ml_model/asthma/predict", methods = ["GET", "POST"])
+def asthma_predict():
+    if request.method == 'POST':
+        form = request.form
+        HeartDisease = form.get('heart-disease')
+        BMI = form.get('bmi')
+        Smoking = form.get('smoking')
+        AlcoholDrinking = form.get('alcohol-drinking')
+        Stroke = form.get('stroke')
+        PhysicalHealth = form.get('physical-health')
+        MentalHealth = form.get('mental-health')
+        DiffWalking = form.get('diff-walking')
+        Sex = form.get('sex')
+        AgeCategory = form.get('age-category')
+        Race = form.get('race')
+        Diabetic = form.get('diabetic')
+        PhysicalActivity = form.get('physical-activity')
+        GenHealth = form.get('gen-health')
+        SleepTime = form.get('sleep-time')
+        
+        arr = [HeartDisease, BMI, Smoking, AlcoholDrinking, Stroke, PhysicalHealth, MentalHealth, DiffWalking, Sex, AgeCategory, Race, Diabetic, PhysicalActivity, GenHealth, SleepTime]
+        
+        df = pd.DataFrame(
+            [arr],
+            columns=['HeartDisease', 'BMI', 'Smoking', 'AlcoholDrinking', 'Stroke', 'PhysicalHealth', 'MentalHealth', 'DiffWalking', 'Sex', 'AgeCategory', 'Race', 'Diabetic', 'PhysicalActivity', 'GenHealth', 'SleepTime']
+        )        
+                
+        with open(os.path.join(ml_models_dir, 'model_asthma')) as f:
+            model_asthma = pickle.load(f)
+            
+    
+    return 'Asthma Predict', 200
+
+
+@app.route("/ml_model/kidney/predict", methods = ["GET", "POST"])
+def kidney_predict():
     if request.method == 'POST':
         form = request.form
         
-    return 'Breast Cancer Predict', 200
+        with open(os.path.join(ml_models_dir, 'model_kidney')) as f:
+            model_kidney = pickle.load(f)
+    
+    return 'Kidney Predict', 200
+
+
+# Skin Cancer
+@app.route("/ml_model/skin/predict", methods = ["GET", "POST"])
+def skin_predict():
+    if request.method == 'POST':
+        form = request.form
+        
+    with open(os.path.join(ml_models_dir, 'model_skin')) as f:
+        model_skin = pickle.load(f)
+    print('loaded the model')
+
+    return 'Skin Predict', 200
